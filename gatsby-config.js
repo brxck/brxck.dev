f530488c6ac3790939815b15dfab90a037566b95
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-01 20:12:45 -0700

    Configure Gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,20 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
+    siteUrl: 'https://brockmcelroy.com',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-json',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Brock McElroy',
+        short_name: 'Brock McElroy',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
@@ -16,6 +22,40 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: '${__dirname}/src/pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: '${__dirname}/src/data',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+              withWebp: true,
+            },
+          },
+          'gatsby-remark-prismjs',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plug-in-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
   ],
 }

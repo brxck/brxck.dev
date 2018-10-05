71413b8033c3312bc944099c06a10dfdfef11f74
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-05 09:26:24 -0700

    Add image directory filesystem source

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [

3392bdf50854efd60a6341354ffcaaddb647fc55
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-05 15:07:57 -0700

    Add card background

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import Header from './Header'
+import Navbar from './Navbar'
 import { StaticQuery, graphql } from 'gatsby'
 import 'normalize.css'
+import '../styles/global.scss'
 import '../styles/typography.scss'
 import style from '../styles/Layout.module.scss'
 
@@ -29,8 +30,9 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Navbar siteTitle={data.site.siteMetadata.title} />
         <div className={style.content}>{children}</div>
+        <div className={style.cardLayout} />
       </>
     )}
   />

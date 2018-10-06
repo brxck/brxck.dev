77f3dfecf151c43a35bde46e00c69a9b0cbe8314
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-06 16:51:10 -0700

    Disable empty contact form send button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import style from '../styles/Contact.module.scss'
 
 class componentName extends Component {
+  state = { email: null, message: null }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  canSubmit() {
+    return this.state.email && this.state.message
+  }
+
   render() {
     return (
       <form action="https://jumprock.co/mail/brxckcontact" method="post">
@@ -10,24 +20,32 @@ class componentName extends Component {
         <div className={style.field}>
           <label htmlFor="email">Your Email</label>
           <input
+            value={this.state.email}
             type="text"
             id="email"
             name="email"
             placeholder="email@example.io"
+            onChange={e => this.handleChange(e)}
           />
         </div>
 
         <div className={style.field}>
           <label htmlFor="message">Message</label>
           <textarea
+            value={this.state.message}
             name="message"
             id="message"
             cols="50"
             rows="7"
             placeholder="What's up?"
+            onChange={e => this.handleChange(e)}
           />
         </div>
-        <button type="submit" className={style.submit}>
+        <button
+          type="submit"
+          className={style.submit}
+          disabled={!this.canSubmit()}
+        >
           Send
         </button>
       </form>

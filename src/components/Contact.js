c85934c137f97ef3e8617551852d6a5a92635ed8
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-08 12:31:14 -0700

    Leverage html5 form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,43 +4,31 @@ import { mdiSend } from '@mdi/js'
 import style from '../styles/Contact.module.scss'
 
 class componentName extends Component {
-  state = { email: null, message: null, subject: null }
-
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  canSubmit() {
-    return this.state.email && this.state.message && this.state.subject
-  }
-
   render() {
     return (
       <form action="https://jumprock.co/mail/brxckcontact" method="post">
-        <input type="text" name="trapit" value="" style={{ display: 'none' }} />
+        <input type="text" name="trapit" style={{ display: 'none' }} />
 
         <div className={style.group}>
           <div className={style.field}>
             <label htmlFor="email">Your Email</label>
             <input
-              value={this.state.email}
-              type="text"
+              type="email"
               id="email"
               name="email"
               placeholder="email@example.io"
-              onChange={e => this.handleChange(e)}
+              required
             />
           </div>
 
           <div className={style.field}>
             <label htmlFor="subject">Subject</label>
             <input
-              value={this.state.subject}
               type="text"
               id="subject"
               name="subject"
               placeholder="Re: Nigerian Prince"
-              onChange={e => this.handleChange(e)}
+              required
             />
           </div>
         </div>
@@ -48,19 +36,17 @@ class componentName extends Component {
         <div className={style.field}>
           <label htmlFor="message">Message</label>
           <textarea
-            value={this.state.message}
             name="message"
             id="message"
             cols="50"
             rows="7"
             placeholder="What's up?"
-            onChange={e => this.handleChange(e)}
+            required
           />
         </div>
         <button
           type="submit"
           className={style.submit}
-          disabled={!this.canSubmit()}
         >
           <Icon
             path={mdiSend}

0671cd3f62a7ce189a5bdb533b1ce8331d076e7b
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-09 08:51:25 -0700

    Animate cards on viewport entry

--- src/components/ScrollingCards.js
+++ src/components/ScrollingCards.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Icon from '@mdi/react'
+import Waypoint from 'react-waypoint'
 import anime from 'animejs'
 import { mdiChevronRight, mdiChevronLeft } from '@mdi/js'
 import style from '../styles/ScrollingCards.module.scss'
 
 class ScrollingCards extends Component {
+  animated = false
+
   constructor(props) {
     super(props)
     this.scrolling = React.createRef()
@@ -12,25 +15,31 @@ class ScrollingCards extends Component {
     this.right = React.createRef()
   }
 
-  componentDidMount() {
-    // Set and animate initial scroll position
-    const scrolling = this.scrolling.current
-    const fadeWidth = scrolling.clientWidth * 0.05
-    const paddingWidth = scrolling.firstChild.clientWidth
-    anime({
-      targets: scrolling,
-      scrollLeft: paddingWidth - fadeWidth,
-    })
-
+  componentDidMount = () => {
     // Hide scrollbar and show buttons if javascript enabled
     if (window.innerWidth > 769) {
-      scrolling.style.overflow = 'hidden'
+      this.scrolling.current.style.overflow = 'hidden'
       this.left.current.style.display = 'block'
       this.right.current.style.display = 'block'
     }
   }
 
-  scroll(amount) {
+  animate = () => {
+    if (this.animated === true) return
+    const scrolling = this.scrolling.current
+    const fadeWidth = scrolling.clientWidth * 0.05
+    const paddingWidth = scrolling.firstChild.clientWidth
+    scrolling.style.willChange = true
+    anime({
+      targets: scrolling,
+      scrollLeft: paddingWidth - fadeWidth,
+      delay: 100,
+      callback: () => (scrolling.style.willChange = false),
+    })
+    this.animated = true
+  }
+
+  scroll = amount => {
     const scrolling = this.scrolling.current
     const current = scrolling.scrollLeft
     const offset = scrolling.firstChild.clientWidth / 4
@@ -49,6 +58,7 @@ class ScrollingCards extends Component {
   render() {
     return (
       <div className={style.container}>
+        <Waypoint onEnter={this.animate} />
         <div className={style.left}>
           <button ref={this.left} onClick={() => this.scroll(-400)}>
             <Icon path={mdiChevronLeft} color="currentColor" />

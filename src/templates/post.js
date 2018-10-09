68def8dc8b9eb5d9c5abbc976a9413a574d06979
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-09 09:16:19 -0700

    Add page title to post layout

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Helmet from 'react-helmet'
+import { graphql, StaticQuery } from 'gatsby'
 import style from '../styles/page.module.scss'
 
 const PostTemplate = ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+      <Helmet title={post.frontmatter.title} />
       <article className={style.text}>
         <h1>{post.frontmatter.title}</h1>
         <h2 className={style.subtitle}>{post.frontmatter.date}</h2>

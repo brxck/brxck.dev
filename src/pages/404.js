434d19b65393afef53d13839d4f6423e656ff4bc
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-21 14:09:42 -0700

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import Img from 'gatsby-image'
+import Link from 'gatsby-link'
 import Layout from '../components/Layout'
+import style from '../styles/page.module.scss'
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">
+      <Img
+        fluid={data.file.childImageSharp.fluid}
+        alt="hero"
+        className={style.hero}
+        critical
+      />
+    </Link>
+    <div className={style.text}>
+      <h1 className={style.section}>404 Not Found</h1>
+      <p>
+        Sorry pal I couldn't find that one, want me to give you a lift back to
+        the <Link to="/">homepage?</Link>
+      </p>
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "car.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 740, quality: 85) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default NotFoundPage

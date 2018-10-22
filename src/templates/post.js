8a0149bcce7afc80ed29817da95306f19d61d669
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-22 09:40:57 -0700

    Use post excerpt for meta description

--- src/templates/post.js
+++ src/templates/post.js
@@ -8,7 +8,15 @@ const PostTemplate = ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
-      <Helmet title={post.frontmatter.title} />
+      <Helmet
+        title={`${post.frontmatter.title} | Brock McElroy`}
+        meta={[
+          {
+            name: 'description',
+            content: post.excerpt,
+          },
+        ]}
+      />
       <article className={style.text}>
         <h1>{post.frontmatter.title}</h1>
         <h2 className={style.subtitle}>{post.frontmatter.date}</h2>
@@ -22,6 +30,7 @@ export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      excerpt
       frontmatter {
         title
         date(formatString: "DD MMMM, YYYY")

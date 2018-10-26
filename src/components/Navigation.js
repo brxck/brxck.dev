a2108fa332da10c8986a1df4bb69c263e0c6a560
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-10-26 16:46:49 -0700

    Add home icon to navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,46 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import SmoothLink from './SmoothLink'
 import style from '../styles/Navigation.module.scss'
 
-const Navigation = () => (
-  <nav className={style.navigation}>
-    <ul>
-      <li>
-        <SmoothLink to="#work">work</SmoothLink>
-      </li>
-      <li>
-        <SmoothLink to="#posts">posts</SmoothLink>
-      </li>
-      <li>
-        <SmoothLink to="#mail">mail</SmoothLink>
-      </li>
-    </ul>
-  </nav>
+const Navigation = props => (
+  <StaticQuery
+    query={graphql`
+      query IconQuery {
+        file(relativePath: { eq: "icon.png" }) {
+          childImageSharp {
+            fixed(width: 32, quality: 85) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <nav className={style.navigation}>
+        <SmoothLink to="/">
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            alt="icon"
+            critical
+            className={style.icon}
+          />
+        </SmoothLink>
+        <ul>
+          <li>
+            <SmoothLink to="#work">work</SmoothLink>
+          </li>
+          <li>
+            <SmoothLink to="#posts">posts</SmoothLink>
+          </li>
+          <li>
+            <SmoothLink to="#mail">mail</SmoothLink>
+          </li>
+        </ul>
+      </nav>
+    )}
+  />
 )
 
 export default Navigation

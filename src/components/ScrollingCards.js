c0a2d45e9409ef69cabf79dd499981952aa68a29
Brock McElroy <28877984+brxck@users.noreply.github.com>
2021-02-13 14:21:28 -0700

    Remove sily animations

--- src/components/ScrollingCards.js
+++ src/components/ScrollingCards.js
@@ -1,40 +1,11 @@
-import React, { useState, useRef } from 'react'
-import Waypoint from 'react-waypoint'
-import anime from 'animejs'
+import React from 'react'
 
 import style from '../styles/ScrollingCards.module.scss'
 
 function ScrollingCards(props) {
-  const scrolling = useRef()
-
-  const [animated, setAnimated] = useState(false)
-
-  function animate() {
-    if (animated === true) return
-    const { current } = scrolling
-    const fadeWidth = current.clientWidth * 0.05
-    const paddingWidth = current.firstChild.clientWidth
-
-    current.style.willChange = true
-    setTimeout(() => {
-        anime({
-            targets: current,
-            scrollLeft: paddingWidth - fadeWidth,
-            delay: 0,
-      
-            callback: () => (current.style.willChange = false),
-          })
-          setAnimated(true)
-    }, 200);
-    setAnimated(true)
-    current.style.willChange = false
-  }
-
   return (
     <div className={style.container}>
-      <Waypoint onEnter={animate} />
-
-      <div ref={scrolling} className={style.scrolling}>
+      <div className={style.scrolling}>
         <div className={style.padding} />
         <>{props.children}</>
         <div className={style.padding} />
